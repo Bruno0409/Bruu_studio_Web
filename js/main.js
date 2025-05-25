223f955585959221305eb1ff823f4844dd17e6d3
bruno0409 <brunooliveiradesign@hotmail.com>
2025-05-25 15:22:11 -0300

    atualizacao

--- js/main.js
+++ js/main.js
@@ -10,11 +10,9 @@
         }, 1);
     };
     spinner();
-    
-    
+
     // Initiate the wowjs
     new WOW().init();
-
 
     // Sticky Navbar
     $(window).scroll(function () {
@@ -24,8 +22,7 @@
             $('.sticky-top').removeClass('shadow-sm').css('top', '-100px');
         }
     });
-    
-    
+
     // Back to top button
     $(window).scroll(function () {
         if ($(this).scrollTop() > 300) {
@@ -39,51 +36,66 @@
         return false;
     });
 
-
     // Modal Video
     var $videoSrc;
     $('.btn-play').click(function () {
         $videoSrc = $(this).data("src");
     });
-    console.log($videoSrc);
     $('#videoModal').on('shown.bs.modal', function (e) {
         $("#video").attr('src', $videoSrc + "?autoplay=1&amp;modestbranding=1&amp;showinfo=0");
-    })
+    });
     $('#videoModal').on('hide.bs.modal', function (e) {
         $("#video").attr('src', $videoSrc);
-    })
+    });
 
-
-    // Project and Testimonial carousel
-    $(".project-carousel, .testimonial-carousel").owlCarousel({
+    // Testimonial carousel (somente ela mantém os navs)
+    $(".testimonial-carousel").owlCarousel({
         autoplay: true,
         smartSpeed: 1000,
         margin: 25,
         loop: true,
         center: true,
         dots: false,
-        nav: true,
+        nav: false,
         navText : [
             '<i class="bi bi-chevron-left"></i>',
             '<i class="bi bi-chevron-right"></i>'
         ],
         responsive: {
-			0:{
-                items:1
-            },
-            576:{
-                items:1
-            },
-            768:{
-                items:2
-            },
-            992:{
-                items:3
-            }
+            0:{ items:1 },
+            576:{ items:1 },
+            768:{ items:2 },
+            992:{ items:3 }
         }
     });
-    
+
 })(jQuery);
 
+
+$(document).ready(function () {
+    var owl = $('.project-carousel,.testimonial-carousel');
+
+    owl.owlCarousel({
+        autoplay: true,
+        loop: true,
+        margin: 30,
+        nav: false, 
+        dots: false,
+        responsive: {
+            0: { items: 1 },
+            768: { items: 2 },
+            992: { items: 3 }
+        }
+    });
+
+    // Botões personalizados
+    $('.btn-prev-project').click(function () {
+        owl.trigger('prev.owl.carousel');
+    });
+
+    $('.btn-next-project').click(function () {
+        owl.trigger('next.owl.carousel');
+    });
+});
 
 
